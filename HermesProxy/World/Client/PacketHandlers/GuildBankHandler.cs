954bd628beafce1297bb8c14f1f4eb16d94f3b77
Kelno <kelnowm@gmail.com>
2022-07-25 20:13:37 +0200

    Implement CMSG_GUILD_BANK_ACTIVATE

---
+++ HermesProxy/World/Client/PacketHandlers/GuildBankHandler.cs
@@ -0,0 +1,57 @@
+﻿using HermesProxy.Enums;
+using HermesProxy.World.Enums;
+using HermesProxy.World.Objects;
+using HermesProxy.World.Server.Packets;
+using System;
+using System.Collections.Generic;
+using static HermesProxy.World.Server.Packets.QueryGuildInfoResponse.GuildInfo;
+
+namespace HermesProxy.World.Client
+{
+    public partial class WorldClient
+    {
+        [PacketHandler(Opcode.SMSG_GUILD_BANK_QUERY_RESULTS, ClientVersionBuild.V2_0_1_6180)]
+        void HandleGuildBankQueryResults(WorldPacket packet)
+        {
+            //_worldPacket << uint64(Money);
+            //_worldPacket << uint8(Tab);
+            //_withdrawalsRemainingPos = _worldPacket.wpos();
+            //_worldPacket << int32(WithdrawalsRemaining);
+            //_worldPacket << uint8(FullUpdate);
+
+            //if (!Tab && FullUpdate)
+            //{
+            //    _worldPacket << uint8(TabInfo.size());
+            //    for (GuildBankTabInfo const& tab : TabInfo)
+            //    {
+            //        _worldPacket << tab.Name;
+            //        _worldPacket << tab.Icon;
+            //    }
+            //}
+
+            //_worldPacket << uint8(ItemInfo.size());
+            //for (GuildBankItemInfo const& item : ItemInfo)
+            //{
+            //    _worldPacket << uint8(item.Slot);
+            //    _worldPacket << uint32(item.ItemID);
+            //    if (item.ItemID)
+            //    {
+            //        _worldPacket << int32(item.RandomPropertiesID);
+            //        if (item.RandomPropertiesID)
+            //            _worldPacket << int32(item.RandomPropertiesSeed);
+
+            //        _worldPacket << uint8(item.Count);
+            //        _worldPacket << int32(item.EnchantmentID);
+            //        _worldPacket << uint8(item.Charges);
+            //        _worldPacket << uint8(item.SocketEnchant.size());
+
+            //        for (GuildBankSocketEnchant const& socketEnchant : item.SocketEnchant)
+            //        {
+            //            _worldPacket << uint8(socketEnchant.SocketIndex);
+            //            _worldPacket << int32(socketEnchant.SocketEnchantID);
+            //        }
+            //    }
+            //}
+        }
+    }
+}
---
+++ HermesProxy/World/Server/PacketHandlers/GuildBankHandler.cs
@@ -0,0 +1,23 @@
+﻿using Framework.Constants;
+using HermesProxy.Enums;
+using HermesProxy.World;
+using HermesProxy.World.Enums;
+using HermesProxy.World.Objects;
+using HermesProxy.World.Server.Packets;
+
+namespace HermesProxy.World.Server
+{
+    public partial class WorldSocket
+    {
+        // Handlers for CMSG opcodes coming from the modern client
+        [PacketHandler(Opcode.CMSG_GUILD_BANK_ACTIVATE)]
+        void HandleGuildBankActivate(GuildBankActivateInfo query)
+        {
+            WorldPacket packet = new WorldPacket(Opcode.CMSG_GUILD_BANK_ACTIVATE);
+            packet.WriteGuid(query.PlayerGuid.To64());
+            packet.WriteBool(query.FullUpdate);
+            SendPacketToServer(packet);
+        }
+
+    }
+}
